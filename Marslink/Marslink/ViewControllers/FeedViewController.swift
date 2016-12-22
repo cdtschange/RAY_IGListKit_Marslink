5bcd0a2d8d3be2a3633f02dd4d11158f00e3992f
wei.mao <wei.mao@jimubox.com>
2016-12-22 17:32:42 +0800

    Creating Your First Section Controller

---
+++ Marslink/Marslink/ViewControllers/FeedViewController.swift
@@ -0,0 +1,78 @@
+//
+//  FeedViewController.swift
+//  Marslink
+//
+//  Created by Mao on 22/12/2016.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+import IGListKit
+
+class FeedViewController: UIViewController {
+    
+    let loader = JournalEntryLoader()
+    
+    // 1
+    let collectionView: IGListCollectionView = {
+        // 2
+        let view = IGListCollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+        // 3
+        view.backgroundColor = UIColor.black
+        return view
+    }()
+    
+    lazy var adapter: IGListAdapter = {
+        return IGListAdapter(updater: IGListAdapterUpdater(), viewController: self, workingRangeSize: 0)
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        loader.loadLatest()
+        
+        view.addSubview(collectionView)
+        
+        adapter.collectionView = collectionView
+        adapter.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension FeedViewController: IGListAdapterDataSource {
+    // 1
+    func objects(for listAdapter: IGListAdapter) -> [IGListDiffable] {
+        return loader.entries
+    }
+    
+    // 2
+    func listAdapter(_ listAdapter: IGListAdapter, sectionControllerFor object: Any) -> IGListSectionController {
+        return JournalSectionController()
+//        return IGListSectionController()
+    }
+    
+    // 3
+    func emptyView(for listAdapter: IGListAdapter) -> UIView? { return nil }
+}
